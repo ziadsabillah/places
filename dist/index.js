872cf7340c0f4684b85809eae2be7bdaaf3bbc41
Ziad Sabillah <ziadoox.sabillah@gmail.com>
2022-07-04 12:26:10 +0100

    implemented google places api

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// const express = require("express");
 const express_1 = __importDefault(require("express"));
+const node_fetch_1 = __importDefault(require("node-fetch"));
+const dotenv_1 = require("dotenv");
+const path_1 = require("path");
+(0, dotenv_1.config)({
+    path: (0, path_1.resolve)(__dirname, "../.env"),
+});
 const app = (0, express_1.default)();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
+const endpoint = "https://maps.googleapis.com/maps/api/place/textsearch/json";
 // define a route handler for the default home page
 app.get("/", (req, res) => {
     res.send("Hello world!");
+});
+app.get("/:query", (req, res) => {
+    const { query } = req.params;
+    // res.send(`Hello ${query}`);
+    const url = `${endpoint}?query=${query}&key=${process.env.API_KEY}`;
+    (0, node_fetch_1.default)(url)
+        .then((response) => response.json())
+        .then((json) => {
+        res.json(json);
+    });
 });
 // start the Express server
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import express from "express";
+import fetch from "node-fetch";
+
+import { config as configDotenv } from "dotenv";
+import { resolve } from "path";
+
+configDotenv({
+  path: resolve(__dirname, "../.env"),
+});
+
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.PORT || 8080; // default port to listen
+
+const endpoint = "https://maps.googleapis.com/maps/api/place/textsearch/json";
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+app.get("/query/:query/:lat/:lng/:region", (req, res) => {
+  const { query, lat, lng, region } = req.params;
+  // res.send(`Hello ${query}`);
+
+  const url = `${endpoint}?query=${query}&location=${lat},${lng}&region=${region}&key=${process.env.API_KEY}`;
+  fetch(url)
+    .then((response: any) => response.json())
+    .then((json: any) => {
+      res.json(json);
+    })
+    .catch((err: any) => {
+      res.status(500).json(err);
+    });
 });
 
 // start the Express server
